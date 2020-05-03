86b0de09dbadfe197b2c49d855f461e32884ec0c
Frederico Carvalho <fred@Fredericos-MacBook-Pro.local>
2020-05-02 23:38:14 -0400

    1/2 javascript done, need do fix css in tab mode in show page.

--- Sprint_2/js/javaScriptBio.js
+++ Sprint_2/js/javaScriptBio.js
@@ -1,71 +1,67 @@
-var peoplesComments = [
-  {
-    //position 0
-    name: "",
-    comment: "",
-  },
-  {
-    //position 1
-    name: "",
-    comment: "",
-  },
-  {
-    //position 2
-    name: "",
-    comment: "",
-  },
-];
+// var peoplesComments = [
+//   {
+//     //position 0
+//     name: "",
+//     comment: "",
+//   },
+//   {
+//     //position 1
+//     name: "",
+//     comment: "",
+//   },
+//   {
+//     //position 2
+//     name: "",
+//     comment: "",
+//   },
+// ];
 
 // function peepsComments() {
 //frame
-let conversationFrame = document.querySelector(".conversation");
+let frame = document.querySelector(".conversation");
 
 //wrap
-let conversationSection = document.createElement("selection");
-conversationSection.classList.add("conversation__section");
-conversationSection.appendChild(conversationFrame);
+let out_put = document.createElement("div");
+out_put.classList.add("output");
+frame.appendChild(out_put);
+
+let conversation = document.createElement("section");
+conversation.classList.add("output__section");
+out_put.appendChild(conversation);
 
 //left side for image
-let conversationSectionLeft = document.createElement("div");
-conversationSectionLeft.classList.add("conversation__section-left");
-conversationSection.appendChild(conversationSectionLeft);
-
+let Left = document.createElement("div");
+Left.classList.add("output__section-left");
+conversation.appendChild(Left);
 //image output
 let outputImg = document.createElement("div");
-outputImg.classList.add("conversation__output-img");
-outputImg.appendChild(conversationSectionLeft);
+outputImg.classList.add("output__output-img");
+Left.appendChild(outputImg);
 
 //right side for name date and comment
-let conversationSectionRight = document.createElement("div");
-conversationSectionRight.classList.add("conversation__section-right");
-conversationSection.appendChild(conversationSectionRight);
+let Right = document.createElement("div");
+Right.classList.add("output__section-right");
+conversation.appendChild(Right);
 
-let conversationSection_Name_Date = document.createElement("div");
-conversationSection_Name_Date.classList.add(
-  "conversation__section-right-nameDate"
-);
-conversationSectionRight.appendChild(conversationSection_Name_Date);
+//name and date
+let Name_Date = document.createElement("div");
+Name_Date.classList.add("output__section-right-nameDate");
+Right.appendChild(Name_Date);
 
-let conversationSectionName = document.createElement("div");
-conversationSectionName.classList.add(
-  "conversation__output-name",
-  "font-paragraphH4-mob"
-);
-conversationSectionRight.appendChild(conversationSectionName);
-conversationSectionName.appendChild(document.createTextNode("NOME DO CARA"));
+//name
+let SectionName = document.createElement("div");
+SectionName.classList.add("output__output-name", "font-paragraphH4-mob");
+Name_Date.appendChild(SectionName);
+SectionName.appendChild(document.createTextNode("Nome do Cara"));
 
-let conversationSectionDate = document.createElement("div");
-conversationSectionDate.classList.add(
-  "conversation__output-date",
-  "font-paragraphH4-mob"
-);
-conversationSectionRight.appendChild(conversationSectionDate);
-conversationSectionDate.appendChild(document.createTextNode("DIA DE ESCREVEU"));
+//date
+let SectionDate = document.createElement("div");
+SectionDate.classList.add("output__output-date", "font-paragraphH4-mob");
+Name_Date.appendChild(SectionDate);
+SectionDate.appendChild(document.createTextNode("Dia que escreveu"));
 
-let conversationComment = document.createElement("h4");
-conversationComment.classList.add(
-  "conversation__output-txt",
-  "font-paragraphH4-mobb"
-);
-conversationSection.appendChild(conversationComment);
-conversationComment.appendChild(document.createTextNode("Commentario Aqui"));
+//comment
+let Comments = document.createElement("h4");
+Comments.classList.add("output__output-txt", "font-paragraphH4-mob");
+Right.appendChild(Comments);
+Comments.appendChild(document.createTextNode("Commentario Aqui"));
--- Sprint_2/js/javaScriptSchedule.js
+++ Sprint_2/js/javaScriptSchedule.js
@@ -1,123 +1,104 @@
-// Hello Everyone :)
-// I am stuck in this problem.
-// I am trying to build the html structure (very similar to this one down here) for the assignment
-// When I append the Child to the parent Node it works.
-// When I am try to add the second Element (child2) which is a Child of the Parent and a sibling Element to child1 it does't work.
-// Any Suggestions?
-
-// <div class="parent">
-//   <div class="child1"></div>
-//   <div class="child1"></div>
-// </div>
-
-// var pops = document.querySelector(".parent");
-// var babyA = document.querySelector(".child1");
-// var babyB = document.querySelector(".child2");
-
-// parent.appendChild(babyA);
-// babyA.nextElementSibling(babyB);
-
 var showSchedule = [
   {
-    //position 0
+    // position 0
     date: "Mon Dec 17 2018",
     venue: "Ronald Lane",
     location: "San Fancisco, CA",
   },
   {
-    //position 1
+    // position 1
     date: "Tue Jul 18 2019",
     venue: "Pier 3 East",
     location: "San Fancisco, CA",
   },
   {
-    //position 2
+    // position 2
     date: "Fri Jul 22 2019",
     venue: "View Loungue",
     location: "San Fancisco, CA",
   },
   {
-    //position 3
+    // position 3
     date: "Sat Aug 12 2019",
     venue: "Hyatt Agency",
     location: "San Fancisco, CA",
   },
   {
-    //position 4
+    // position 4
     date: "Fri Sep 05 2019",
     venue: "Moscow Center",
     location: "San Fancisco, CA",
   },
   {
-    //position 5
+    // position 5
     date: "Wed Aug 11 2019",
     venue: "Pres Club",
     location: "San Fancisco, CA",
   },
 ];
 
-let showDate = document.querySelector(".shows__date");
-let showVenue = document.querySelector(".shows__venue");
-let showLocation = document.querySelector(".shows__location");
+for (i = 0; i <= showSchedule.length - 1; i++) {
+  var dataShow = showSchedule[i].date;
+  var venueShow = showSchedule[i].venue;
+  var venueLocation = showSchedule[i].location;
 
-// function ShowSchedule() {
+  let scheduleFrame = document.querySelector(".shows__schedules-frame");
 
-let scheduleFrame = document.querySelector(".shows__schedules-frame");
+  // Date
+  let dateWrap = document.createElement("div");
+  dateWrap.classList.add("shows__date-wrap");
+  scheduleFrame.appendChild(dateWrap);
 
-// Date
-let dateWrap = document.createElement("div");
-dateWrap.classList.add("shows__date-wrap");
-let dateTitle = document.createElement("h4");
-dateTitle.classList.add("font-listContactInfoH4-mob", "shows__date-title");
-dateTitle.appendChild(document.createTextNode("DATE"));
-let dateAnnounced = document.createElement("h4");
-dateAnnounced.classList.add("font-listContactInfoH4-mob", "shows__date");
+  let dateTitle = document.createElement("h4");
+  dateTitle.classList.add("font-listContactInfoH4-mob", "shows__date-title");
+  dateWrap.appendChild(dateTitle);
+  dateTitle.appendChild(document.createTextNode("DATE"));
 
-scheduleFrame.appendChild(dateWrap);
-dateWrap.appendChild(dateTitle);
-// dateAnnounced.appendChild();
+  let dateAnnounced = document.createElement("h4");
+  dateAnnounced.classList.add("font-listContactInfoH4-mob", "shows__date");
+  dateWrap.appendChild(dateAnnounced);
+  dateAnnounced.appendChild(document.createTextNode(dataShow)); // posicao da data
 
-//Venue
-let venueWrap = document.createElement("div");
-venueWrap.classList.add("shows__venue-wrap");
-let venueTitle = document.createElement("h4");
-venueTitle.classList.add("font-listContactInfoH4-mob", "shows__venue-title");
-venueTitle.appendChild(document.createTextNode("VENUE"));
-let venueAnnounced = document.createElement("h4");
-venueAnnounced.classList.add("font-listContactInfoH4-mob", "shows__venue");
+  //Venue
+  let venueWrap = document.createElement("div");
+  venueWrap.classList.add("shows__venue-wrap");
+  scheduleFrame.appendChild(venueWrap);
 
-scheduleFrame.appendChild(venueWrap);
-venueWrap.appendChild(venueTitle);
-// venueAnnounced.appendChild();
+  let venueTitle = document.createElement("h4");
+  venueTitle.classList.add("font-listContactInfoH4-mob", "shows__venue-title");
+  venueWrap.appendChild(venueTitle);
+  venueTitle.appendChild(document.createTextNode("VENUE"));
 
-//Location
-let locationWrap = document.createElement("div");
-locationWrap.classList.add("shows__location-wrap");
-let locationTitle = document.createElement("h4");
-locationTitle.classList.add(
-  "font-listContactInfoH4-mob",
-  "shows__location-title"
-);
-locationTitle.appendChild(document.createTextNode("LOCATION"));
-let locationAnnounced = document.createElement("h4");
-locationAnnounced.classList.add(
-  "font-listContactInfoH4-mob",
-  "shows__location"
-);
+  let venueAnnounced = document.createElement("h4");
+  venueAnnounced.classList.add("font-listContactInfoH4-mob", "shows__venue");
+  venueWrap.appendChild(venueAnnounced);
+  venueAnnounced.appendChild(document.createTextNode(venueShow)); // posicao do venue
 
-scheduleFrame.appendChild(locationWrap);
-locationWrap.appendChild(locationTitle);
-// locationAnnounced.appendChild = showSchedule[0].location;
+  //Location
+  let locationWrap = document.createElement("div");
+  locationWrap.classList.add("shows__location-wrap");
+  scheduleFrame.appendChild(locationWrap);
 
-// button
-let buyTickedBtn = document.createElement("button");
-buyTickedBtn.classList.add("btn", "shows__button");
-buyTickedBtn.appendChild(document.createTextNode("BUY TICKET"));
+  let locationTitle = document.createElement("h4");
+  locationTitle.classList.add(
+    "font-listContactInfoH4-mob",
+    "shows__location-title"
+  );
+  locationWrap.appendChild(locationTitle);
+  locationTitle.appendChild(document.createTextNode("LOCATION"));
 
-scheduleFrame.appendChild(buyTickedBtn);
+  let locationAnnounced = document.createElement("h4");
+  locationAnnounced.classList.add(
+    "font-listContactInfoH4-mob",
+    "shows__location"
+  );
+  locationWrap.appendChild(locationAnnounced);
+  locationAnnounced.appendChild(document.createTextNode(venueLocation)); // posicao do local
 
-// }
+  //button
+  let buyTickedBtn = document.createElement("button");
+  buyTickedBtn.classList.add("btn", "shows__button");
+  buyTickedBtn.appendChild(document.createTextNode("BUY TICKET"));
 
-// showDate.innerHTML = showSchedule[0].date;
-// showVenue.innerHTML = showSchedule[0].venue;
-// showLocation.innerHTML = showSchedule[0].location;
+  scheduleFrame.appendChild(buyTickedBtn);
+}
