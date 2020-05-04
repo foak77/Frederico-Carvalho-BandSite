b1fdfb7cf337d137b81cb8817002aac046413092
Frederico Carvalho <fred@Fredericos-MacBook-Pro.local>
2020-05-03 20:00:26 -0400

    istructions folders deleted javascrip updated and css updated

--- Sprint_2/js/javaScriptBio.js
+++ Sprint_2/js/javaScriptBio.js
@@ -1,67 +1,153 @@
-// var peoplesComments = [
-//   {
-//     //position 0
-//     name: "",
-//     comment: "",
-//   },
-//   {
-//     //position 1
-//     name: "",
-//     comment: "",
-//   },
-//   {
-//     //position 2
-//     name: "",
-//     comment: "",
-//   },
-// ];
+var peoplesComments = [
+  {
+    name: "FRED IS SORRY :(",
+    dateWritten: "12/18/2018",
+    comment:
+      "I COUDN'T INSERT THE COMMENT BEFORE THESE PRE EXISTING COMMENTS - TRYED HARD...HOPE DANNIL GOES THROUGH THIS STEP, BUT THEY DO GET INSERTED AT THE BOTTOM :)",
+    photo: "ph",
+  },
+  {
+    name: "Gary Wong",
+    dateWritten: "12/12/2018",
+    comment:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+    photo: "ph",
+  },
+  {
+    name: "Theodore Duncan",
+    dateWritten: "11/15/2018",
+    comment:
+      "How can someone be so good!!!You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+    photo: "ph",
+  },
+];
 
-// function peepsComments() {
-//frame
-let frame = document.querySelector(".conversation");
+function commentPresentation(
+  commentName,
+  commentDate,
+  commentInfo,
+  commentPhoto
+) {
+  //frame
+  let frame = document.querySelector(".conversation");
 
-//wrap
-let out_put = document.createElement("div");
-out_put.classList.add("output");
-frame.appendChild(out_put);
+  //wrap
+  let out_put = document.createElement("div");
+  out_put.classList.add("output");
+  frame.appendChild(out_put);
 
-let conversation = document.createElement("section");
-conversation.classList.add("output__section");
-out_put.appendChild(conversation);
+  let conversation = document.createElement("section");
+  conversation.classList.add("output__section");
+  out_put.appendChild(conversation);
 
-//left side for image
-let Left = document.createElement("div");
-Left.classList.add("output__section-left");
-conversation.appendChild(Left);
-//image output
-let outputImg = document.createElement("div");
-outputImg.classList.add("output__output-img");
-Left.appendChild(outputImg);
+  //left side for image
+  let Left = document.createElement("div");
+  Left.classList.add("output__section-left");
+  conversation.appendChild(Left);
 
-//right side for name date and comment
-let Right = document.createElement("div");
-Right.classList.add("output__section-right");
-conversation.appendChild(Right);
+  //image output
+  let outputImg = document.createElement("div");
+  outputImg.classList.add("output__output-img");
+  Left.appendChild(outputImg); //picture of the guy
 
-//name and date
-let Name_Date = document.createElement("div");
-Name_Date.classList.add("output__section-right-nameDate");
-Right.appendChild(Name_Date);
+  //right side for name date and comment
+  let Right = document.createElement("div");
+  Right.classList.add("output__section-right");
+  conversation.appendChild(Right);
 
-//name
-let SectionName = document.createElement("div");
-SectionName.classList.add("output__output-name", "font-paragraphH4-mob");
-Name_Date.appendChild(SectionName);
-SectionName.appendChild(document.createTextNode("Nome do Cara"));
+  //name and date
+  let Name_Date = document.createElement("div");
+  Name_Date.classList.add("output__section-right-nameDate");
+  Right.appendChild(Name_Date);
 
-//date
-let SectionDate = document.createElement("div");
-SectionDate.classList.add("output__output-date", "font-paragraphH4-mob");
-Name_Date.appendChild(SectionDate);
-SectionDate.appendChild(document.createTextNode("Dia que escreveu"));
+  //name
+  let SectionName = document.createElement("div");
+  SectionName.classList.add("output__output-name", "font-paragraphH4-mob");
+  Name_Date.appendChild(SectionName);
+  SectionName.appendChild(document.createTextNode(commentName)); // Nome do Cara
 
-//comment
-let Comments = document.createElement("h4");
-Comments.classList.add("output__output-txt", "font-paragraphH4-mob");
-Right.appendChild(Comments);
-Comments.appendChild(document.createTextNode("Commentario Aqui"));
+  //date
+  let SectionDate = document.createElement("div");
+  SectionDate.classList.add("output__output-date", "font-paragraphH4-mob");
+  Name_Date.appendChild(SectionDate);
+  SectionDate.appendChild(document.createTextNode(commentDate)); // Data que foi escrita
+
+  //comment
+  let Comments = document.createElement("h4");
+  Comments.classList.add("output__output-txt", "font-paragraphH4-mob");
+  Right.appendChild(Comments);
+  Comments.appendChild(document.createTextNode(commentInfo)); // O comentario
+}
+
+// FIRST OBJECT OF THE ARRAY
+cN = peoplesComments[0].name;
+cD = peoplesComments[0].dateWritten;
+cI = peoplesComments[0].comment;
+cP = peoplesComments[0].photo;
+commentPresentation(cN, cD, cI, cP);
+
+cN = peoplesComments[1].name;
+cD = peoplesComments[1].dateWritten;
+cI = peoplesComments[1].comment;
+cP = peoplesComments[1].photo;
+commentPresentation(cN, cD, cI, cP);
+
+cN = peoplesComments[2].name;
+cD = peoplesComments[2].dateWritten;
+cI = peoplesComments[2].comment;
+cP = peoplesComments[2].photo;
+commentPresentation(cN, cD, cI, cP);
+
+//DATE
+let clickOfDate = new Date();
+let day = clickOfDate.getMonth() + 1;
+let month = clickOfDate.getDate();
+let year = clickOfDate.getFullYear();
+var cD = day + "/" + month + "/" + year;
+
+var cN;
+var cI;
+var cP = "PH";
+
+var formulario = document.querySelector(".conversation__form");
+
+// EVENT LISTENER
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //SELECTING THE ELEMENTS IN THE DOM AND ASSIGNING INTO VARIABLES
+  let SectionName = document.querySelector(".conversation__input-name");
+  let SectionComment = document.querySelector(
+    ".conversation__form-input-comment"
+  );
+
+  //ASSIGNING THE "VALUE" FROM THE INPUT INTO VARIABLES
+  cN = SectionName.value;
+  cI = SectionComment.value;
+
+  //CREATING A NEW OBJECT
+  var newComment = {
+    name: cN,
+    dateWritten: cD, //LINE 102 TO 106
+    comment: cI,
+    photo: "ph",
+  };
+
+  //PUSHING THE NEW OBJECT IN THE ARRAY - LAST
+  peoplesComments.push(newComment);
+
+  //UNSHIFT WILL PUT THE OBJECT IN THE FIRST POSITION
+  //peoplesComments.unshift(newComment); BUT STILL DOESN'T WORK. I STILL DON'T KNOW THE METHOD. appendBefore()?
+
+  //FINDING THE LAST ELEMENT IN THE ARRAY
+  var i = peoplesComments.length - 1;
+
+  //ASSIGNING THE RESULTS
+  cN = peoplesComments[i].name;
+  cD = peoplesComments[i].dateWritten;
+  cI = peoplesComments[i].comment;
+  cP = peoplesComments[i].photo;
+  commentPresentation(cN, cD, cI, cP);
+
+  formulario.reset();
+});
--- Sprint_2/js/javaScriptSchedule.js
+++ Sprint_2/js/javaScriptSchedule.js
@@ -1,104 +1,126 @@
+let scheduleFrame = document.querySelector(".shows__schedules-frame");
 var showSchedule = [
   {
     // position 0
     date: "Mon Dec 17 2018",
     venue: "Ronald Lane",
     location: "San Fancisco, CA",
+    tickets: "BUY - SELLIN FAST",
   },
   {
     // position 1
     date: "Tue Jul 18 2019",
     venue: "Pier 3 East",
     location: "San Fancisco, CA",
+    tickets: "- SOLD OUT -",
   },
   {
     // position 2
     date: "Fri Jul 22 2019",
     venue: "View Loungue",
     location: "San Fancisco, CA",
+    tickets: "BUY - CHARITY EVENT",
   },
   {
     // position 3
     date: "Sat Aug 12 2019",
     venue: "Hyatt Agency",
     location: "San Fancisco, CA",
+    tickets: "BUY TICKET",
   },
   {
     // position 4
     date: "Fri Sep 05 2019",
     venue: "Moscow Center",
     location: "San Fancisco, CA",
+    tickets: "BUY - AWESOME MUSIC/VENUE",
   },
   {
     // position 5
     date: "Wed Aug 11 2019",
     venue: "Pres Club",
     location: "San Fancisco, CA",
+    tickets: "BUY TICKET",
   },
 ];
 
 for (i = 0; i <= showSchedule.length - 1; i++) {
-  var dataShow = showSchedule[i].date;
-  var venueShow = showSchedule[i].venue;
-  var venueLocation = showSchedule[i].location;
+  let dS = showSchedule[i].date; //day
+  let vS = showSchedule[i].venue; //venue
+  let lS = showSchedule[i].location; //local
+  let tP = showSchedule[i].tickets; // future function to buy
 
-  let scheduleFrame = document.querySelector(".shows__schedules-frame");
+  function showSchedulePresentation(
+    dataShow,
+    venueShow,
+    locationShow,
+    ticketProcedure
+  ) {
+    //wrapping
+    let wrapping = document.createElement("div");
+    wrapping.classList.add("shows__wrapping");
+    scheduleFrame.appendChild(wrapping);
 
-  // Date
-  let dateWrap = document.createElement("div");
-  dateWrap.classList.add("shows__date-wrap");
-  scheduleFrame.appendChild(dateWrap);
+    // Date
+    let dateWrap = document.createElement("div");
+    dateWrap.classList.add("shows__date-wrap");
+    wrapping.appendChild(dateWrap);
 
-  let dateTitle = document.createElement("h4");
-  dateTitle.classList.add("font-listContactInfoH4-mob", "shows__date-title");
-  dateWrap.appendChild(dateTitle);
-  dateTitle.appendChild(document.createTextNode("DATE"));
+    let dateTitle = document.createElement("h4");
+    dateTitle.classList.add("font-listContactInfoH4-mob", "shows__date-title");
+    dateWrap.appendChild(dateTitle);
+    dateTitle.appendChild(document.createTextNode("DATE"));
 
-  let dateAnnounced = document.createElement("h4");
-  dateAnnounced.classList.add("font-listContactInfoH4-mob", "shows__date");
-  dateWrap.appendChild(dateAnnounced);
-  dateAnnounced.appendChild(document.createTextNode(dataShow)); // posicao da data
+    let dateAnnounced = document.createElement("h4");
+    dateAnnounced.classList.add("font-listContactInfoH4-mob", "shows__date");
+    dateWrap.appendChild(dateAnnounced);
+    dateAnnounced.appendChild(document.createTextNode(dataShow)); // DATE
 
-  //Venue
-  let venueWrap = document.createElement("div");
-  venueWrap.classList.add("shows__venue-wrap");
-  scheduleFrame.appendChild(venueWrap);
+    //Venue
+    let venueWrap = document.createElement("div");
+    venueWrap.classList.add("shows__venue-wrap");
+    wrapping.appendChild(venueWrap);
 
-  let venueTitle = document.createElement("h4");
-  venueTitle.classList.add("font-listContactInfoH4-mob", "shows__venue-title");
-  venueWrap.appendChild(venueTitle);
-  venueTitle.appendChild(document.createTextNode("VENUE"));
+    let venueTitle = document.createElement("h4");
+    venueTitle.classList.add(
+      "font-listContactInfoH4-mob",
+      "shows__venue-title"
+    );
+    venueWrap.appendChild(venueTitle);
+    venueTitle.appendChild(document.createTextNode("VENUE"));
 
-  let venueAnnounced = document.createElement("h4");
-  venueAnnounced.classList.add("font-listContactInfoH4-mob", "shows__venue");
-  venueWrap.appendChild(venueAnnounced);
-  venueAnnounced.appendChild(document.createTextNode(venueShow)); // posicao do venue
+    let venueAnnounced = document.createElement("h4");
+    venueAnnounced.classList.add("font-listContactInfoH4-mob", "shows__venue");
+    venueWrap.appendChild(venueAnnounced);
+    venueAnnounced.appendChild(document.createTextNode(venueShow)); // VENUNE NAME
 
-  //Location
-  let locationWrap = document.createElement("div");
-  locationWrap.classList.add("shows__location-wrap");
-  scheduleFrame.appendChild(locationWrap);
+    //Location
+    let locationWrap = document.createElement("div");
+    locationWrap.classList.add("shows__location-wrap");
+    wrapping.appendChild(locationWrap);
 
-  let locationTitle = document.createElement("h4");
-  locationTitle.classList.add(
-    "font-listContactInfoH4-mob",
-    "shows__location-title"
-  );
-  locationWrap.appendChild(locationTitle);
-  locationTitle.appendChild(document.createTextNode("LOCATION"));
+    let locationTitle = document.createElement("h4");
+    locationTitle.classList.add(
+      "font-listContactInfoH4-mob",
+      "shows__location-title"
+    );
+    locationWrap.appendChild(locationTitle);
+    locationTitle.appendChild(document.createTextNode("LOCATION"));
 
-  let locationAnnounced = document.createElement("h4");
-  locationAnnounced.classList.add(
-    "font-listContactInfoH4-mob",
-    "shows__location"
-  );
-  locationWrap.appendChild(locationAnnounced);
-  locationAnnounced.appendChild(document.createTextNode(venueLocation)); // posicao do local
+    let locationAnnounced = document.createElement("h4");
+    locationAnnounced.classList.add(
+      "font-listContactInfoH4-mob",
+      "shows__location"
+    );
+    locationWrap.appendChild(locationAnnounced);
+    locationAnnounced.appendChild(document.createTextNode(locationShow)); // LOCATION
 
-  //button
-  let buyTickedBtn = document.createElement("button");
-  buyTickedBtn.classList.add("btn", "shows__button");
-  buyTickedBtn.appendChild(document.createTextNode("BUY TICKET"));
-
-  scheduleFrame.appendChild(buyTickedBtn);
+    //button
+    let buyTickedBtn = document.createElement("button");
+    buyTickedBtn.classList.add("btn", "shows__button");
+    buyTickedBtn.appendChild(document.createTextNode(tP));
+    wrapping.appendChild(buyTickedBtn);
+    //future function to buy ticket event click/submit
+  }
+  showSchedulePresentation(dS, vS, lS, tP);
 }
